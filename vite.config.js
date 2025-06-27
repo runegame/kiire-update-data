7bf70c611a3978fd728ed31e020f36593c5e74fd
Angel <alm93alm@gmail.com>
2025-06-27 13:00:22 -0500

    Build

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,49 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools()
-  ],
+  plugins: [vue(), vueDevTools()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true
+        })
+      ]
+    }
+  },
   define: {
-    'process.env': {},
-    'process.env.NODE_ENV': JSON.stringify('production')
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.env': {} // cubre otros usos generales
   },
   build: {
     lib: {
       entry: fileURLToPath(new URL('./src/main.js', import.meta.url)),
-      name: 'MyVueApp', // nombre global (evita conflicto)
+      name: 'MyVueApp',
       formats: ['iife'],
       fileName: () => 'kiire-update-info.js'
     },
+    minify: 'terser',
+    terserOptions: {
+      mangle: {
+        toplevel: true
+      }
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
   },
   resolve: {
     alias: {
